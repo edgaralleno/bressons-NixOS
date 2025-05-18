76802b46237f744e52977c60263dc58e35c5302b
edgaralleno <natecolemiller@gmail.com>
2025-05-18 01:47:02 -0400

    scripting test`

---
+++ copy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# script that should copy my config files
+#
+
+rm -r hypr/ nixos/ waybar/
+cp -r ~/.config/hypr/ ~/.config/waybar/ /etc/nixos/ -t ~/Storage/Nextcloud/config_bak/
+git add .
+git commit
+git push -u origin main
+echo "edgaralleno"
+echo "ghp_djRIGEnCyIa8ILTTUVpukGtQbVbLubLub0IwrJJ"
