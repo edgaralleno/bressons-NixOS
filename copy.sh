cb3887fb9abc496db19869163b934b78750bc181
edgaralleno <natecolemiller@gmail.com>
2025-05-28 20:27:06 -0400

    Modularized my hyprconfig.

--- copy.sh
+++ copy.sh
@@ -3,10 +3,8 @@
 # script that should copy my config files
 #
 
-rm -r ~/Storage/Nextcloud/config_bak/hypr/ ~/Storage/Nextcloud/config_bak/nixos/ ~/Storage/Nextcloud/config_bak/waybar/
-cp -r ~/.config/hypr/ ~/.config/waybar/ /etc/nixos/ -t ~/Storage/Nextcloud/config_bak/
-cp -r ~/.cache/wal/ -t ~/Storage/Nextcloud/config_bak/wal_cache/
-cp -r ~/.config/wal/ -t ~/Storage/Nextcloud/config_bak/wal_config/
+rm -r /home/natem/Storage/Nextcloud/config_bak/hypr/ /home/natem/Storage/Nextcloud/config_bak/nixos/ /home/natem/Storage/Nextcloud/config_bak/waybar/ /home/natem/Storage/Nextcloud/config_bak/wal /home/natem/Storage/Nextcloud/config_bak/ncmpcpp /home/natem/Storage/Nextcloud/config_bak/kitty
+cp -r /home/natem/.config/hypr/ /home/natem/.config/waybar/ /etc/nixos/ /home/natem/.config/wal/ /home/natem/.config/ncmpcpp/ /home/natem/.config/kitty/ -t /home/natem/Storage/Nextcloud/config_bak/
 # git add .
 # git commit
 # git push -u origin main
